7e5cde8695678f962d637778d949d352991878dd
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-02-03 17:06:14 +0800

    some shell

---
+++ setEnv.sh
@@ -0,0 +1,31 @@
+#install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+#install vsftpd and open ports
+yum -y install vsftpd
+firewall-cmd --zone=public --add-port=21/tcp --permanent
+firewall-cmd --zone=public --add-port=3000/tcp --permanent
+firewall-cmd --permanent --zone=public --add-service=ftp
+firewall-cmd --reload
+systemctl start vsftpd.service
+
+
+#install node
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
+npm install -g pm2
+
+#install ffmpeg
+yum -y install epel-release
+rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
+yum -y install ffmpeg ffmpeg-devel
+
+
+#install git 
+yum -y install git
+git clone https://github.com/luoyuxiang61/youtube-thunder.git
+cd youtube-thunder
+npm install
+pm2 start app.js
