c55d9e5c02a8644ea18db76ef08ba755d93379f1
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-02-06 15:38:33 +0800

    fix a string bug in 1080

--- functions.js
+++ functions.js
@@ -2,25 +2,25 @@ const { exec } = require('child_process')
 
 //list all audio and video formats 
 function getVideoInfo(url) {
-    return new Promise((resolve,reject) => {
-        exec(`youtube-dl --list-formats ${url}`, (error,stdout,stderr) => {
-            if(error)reject(error)
-            if(stderr)reject(error)
-            resolve(convertFormats(stdout,url))
+    return new Promise((resolve, reject) => {
+        exec(`youtube-dl --list-formats ${url}`, (error, stdout, stderr) => {
+            if (error) reject(error)
+            if (stderr) reject(error)
+            resolve(convertFormats(stdout, url))
         })
     })
 }
 
 //convert formats infomation to array
-function convertFormats(stdout,url) {
+function convertFormats(stdout, url) {
     let formatsArr = stdout.split('\n').slice(5)
-    let audioN = formatsArr.filter((item) => item.indexOf('m4a') != -1 ).map((item) => parseInt(item.substring(0,3)))[0]
+    let audioN = formatsArr.filter((item) => item.indexOf('m4a') != -1).map((item) => parseInt(item.substring(0, 3)))[0]
     let bestVideoInfo = formatsArr.filter((item) => item.indexOf('mp4') != -1 && item.indexOf('video only') != -1).pop()
-    let bestVideoP = bestVideoInfo.substr(35,5).trim()
-    let bestVideoM = bestVideoInfo.substring(bestVideoInfo.length - 9).replace(/,/,' ').trim()
-    let bestVideoN = parseInt(formatsArr.filter((item) => item.indexOf('mp4') != -1 && item.indexOf('video only') != -1).pop().substring(0,3))
+    let bestVideoP = bestVideoInfo.substr(35, 5).trim()
+    let bestVideoM = bestVideoInfo.substring(bestVideoInfo.length - 9).replace(/,/, ' ').trim()
+    let bestVideoN = parseInt(formatsArr.filter((item) => item.indexOf('mp4') != -1 && item.indexOf('video only') != -1).pop().substring(0, 3))
     let audioVideoInfo = formatsArr.filter(item => item.indexOf('best') != -1)
-    let audioVideoN = formatsArr.filter(item => item.indexOf('best') != -1).map((item) => parseInt(item.substring(0,3)))[0]
+    let audioVideoN = formatsArr.filter(item => item.indexOf('best') != -1).map((item) => parseInt(item.substring(0, 3)))[0]
     return {
         audioN,
         bestVideoN,
@@ -32,34 +32,34 @@ function convertFormats(stdout,url) {
 
 //download 720p
 function download720(url) {
-    return new Promise((resolve,reject) => {
-        console.log(`*************** download the hd video now! ******************`);   
-        exec(`cd /var/ftp && youtube-dl --no-playlist -f best -o '%(title)s.%(ext)s' ${url}`, (error,stdout,stderr) => {
-            if(error)reject(error)
-            if(stderr)reject(stderr)
+    return new Promise((resolve, reject) => {
+        console.log(`*************** download the hd video now! ******************`);
+        exec(`cd /var/ftp && youtube-dl --no-playlist -f best -o '%(title)s.%(ext)s' ${url}`, (error, stdout, stderr) => {
+            if (error) reject(error)
+            if (stderr) reject(stderr)
             resolve(stdout)
         })
     })
 }
 
 //download a video 
-function download1080({bestVideoN, audioN, url}) {
-    return new Promise((resolve,reject) => {
-            console.log(`$$$$$$$$$$$$$$$$$$$ download the best video now! $$$$$$$$$$$$$$$$$$$`)
-            exec(`cd /var/ftp && youtube-dl --no-playlist -f " + bestVideoN + "+" + audioN + " -o '%(title)s.%(ext)s' ${url}`, (error,stdout,stderr) => {
-                if(error)reject(error)
-                if(stderr)reject(stderr)
-                resolve(stdout)
-            }) 
+function download1080({ bestVideoN, audioN, url }) {
+    return new Promise((resolve, reject) => {
+        console.log(`$$$$$$$$$$$$$$$$$$$ download the best video now! $$$$$$$$$$$$$$$$$$$`)
+        exec(`cd /var/ftp && youtube-dl --no-playlist -f ${bestVideoN}+${audioN} -o '%(title)s.%(ext)s' ${url}`, (error, stdout, stderr) => {
+            if (error) reject(error)
+            if (stderr) reject(stderr)
+            resolve(stdout)
+        })
     })
 }
 
 function downloadList(url) {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         console.log(`###################### download the list now! ######################`)
-        exec(`cd /var/ftp && youtube-dl --yes-playlist -f best -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' ${url}`, (error,stdout,stderr) => {
-            if(error)reject(error)
-            if(stderr)reject(stderr)
+        exec(`cd /var/ftp && youtube-dl --yes-playlist -f best -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' ${url}`, (error, stdout, stderr) => {
+            if (error) reject(error)
+            if (stderr) reject(stderr)
             resolve(stdout)
         })
 
@@ -68,11 +68,11 @@ function downloadList(url) {
 
 //dangerous function delete all videos on ftp server
 function deleteAllVideos() {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         console.log(`!!!!!!!!!!!!!!!!!!!!!!!Delete all the videos!!!!!!!!!!!!!!!!!!!!`)
-        exec(`cd /var/ftp && pwd && rm -rf *`, (error,stdout,stderr) => {
-            if(error) reject(error)
-            if(stderr) reject(stderr)
+        exec(`cd /var/ftp && pwd && rm -rf *`, (error, stdout, stderr) => {
+            if (error) reject(error)
+            if (stderr) reject(stderr)
             resolve(`${stdout} ok all the videos are deleted now , it's much more clean , you can download more videos haha`)
         })
     })
