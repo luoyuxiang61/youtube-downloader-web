e0862b4e39aada0c11418be3ba81d8e9d7c7a39d
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-03-07 13:38:10 +0800

    https

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const fs = require('fs')
 const request = require('request');
-const http = require('http')
+const https = require('https')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 const { getVideoInfo, download720, download1080, downloadList, deleteAllVideos, deleteOneVideo, decodeUrl, getRealUrl, getVideoInfo2 } = require('./functions')
-
-app.use((req, res, next) => {
-    //     res.setHeader('Access-Control-Allow-Origin', '*')
-    next()
-})
-
 
 app.get('/', (req, res) => {
     res.send('hello, world');
@@ -53,7 +47,7 @@ app.get('/geekshine', (req, res) => {
 
 app.get('/geekshine2', (req, res) => {
     getRealUrl(decodeUrl(req.query.url)).then(realUrl => {
-        http.get(realUrl, (videoStream) => {
+        https.get(realUrl, (videoStream) => {
             let contentLength = videoStream.get('Content-Length')
             let contentDisposition = videoStream.get('Content-Disposition')
             // res.set({
